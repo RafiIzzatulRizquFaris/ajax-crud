d6e167bc257acee5e572b64ac95544de658e6dfd
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2020-06-03 19:30:47 +0700

    add show_data.php to show all data from database

---
+++ show_data.php
@@ -0,0 +1,14 @@
+<?php
+include('koneksi.php');
+$connection = openConn();
+
+$query = "SELECT * FROM `tbl_siswa`";
+
+$response = array();
+$result = mysqli_query($connection, $query);
+while ($row = mysqli_fetch_assoc($result)) {
+    $response[] = $row;
+}
+header('json app');
+echo json_encode($response);
+?>
